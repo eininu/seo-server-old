c3da383361a7cb61fbef0a9b34baece0546db1c2
ein <ein@jp9.org>
2022-12-20 10:27:48 -0800

    Add checking if websites table exists

--- routes/websites.js
+++ routes/websites.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const { dbAll } = require("../database/database");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  // checking that table websites exists in database
+  let tableExistings = await dbAll(
+    `SELECT name FROM sqlite_master WHERE type='table' AND name='websites';`
+  );
+  tableExistings = tableExistings.length;
+
+  if (tableExistings === 0) {
+    return res.send({ message: "The websites api isn't configured" });
+  }
+
   res.send({ message: "Websites path" });
 });
 
