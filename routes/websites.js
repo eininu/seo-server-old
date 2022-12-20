bc4d0e6e589d5d998e7d28860dfa36053a2be73b
ein <ein@jp9.org>
2022-12-20 15:10:03 -0800

    Add checking if websites table exists

--- routes/websites.js
+++ routes/websites.js
@@ -1,19 +1,37 @@
 const express = require("express");
-const { dbAll } = require("../database/database");
+const { dbRun, dbAll } = require("../database/database");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  // checking that table websites exists in database
+const checkIfTableExists = async () => {
   let tableExistings = await dbAll(
     `SELECT name FROM sqlite_master WHERE type='table' AND name='websites';`
   );
-  tableExistings = tableExistings.length;
+
+  return tableExistings.length;
+};
+router.get("/", async (req, res) => {
+  // checking that table websites exists in database
+
+  let tableExistings = await checkIfTableExists();
 
   if (tableExistings === 0) {
     return res.send({ message: "The websites api isn't configured" });
   }
 
-  res.send({ message: "Websites path" });
+  return res.send({ message: "ok" });
+});
+
+router.get("/create_table", async (req, res) => {
+  let tableExistings = await checkIfTableExists();
+
+  if (tableExistings === 1) {
+    return res.send({ message: "App currently configured" });
+  }
+
+  // dbRun(`DROP TABLE IF EXISTS websites`);
+  await dbRun(`CREATE TABLE IF NOT EXISTS websites(website PRIMARY KEY)`);
+
+  return res.send({ message: "Database table created successfully" });
 });
 
 module.exports = router;
