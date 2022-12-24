52e8dd4d7463057d7eb4cb9d88c46c4ec17cc2c4
ein <ein@jp9.org>
2022-12-24 06:13:26 -0800

    Add basic infrastructure to handle synchronisation between server

--- routes/sync/websites.js
+++ routes/sync/websites.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  res.send({ message: "" });
+// verify connection between webservers
+// if websites connected - check connection between
+const verifyConnection = () => {
+  return true;
+};
+
+// compare websites between remote servers
+// to get websites that should be added to current servers
+router.post("/compare_websites", (req, res) => {
+  verifyConnection();
+
+  res.send({ message: "compare_websites" });
+});
+
+// download missing websites on the local server
+// transfer missing websites to remote servers
+router.post("/all", (req, res) => {
+  verifyConnection();
+  // check missing websites on local
+  res.send({ message: "compare_websites" });
+});
+
+// add website to all remote servers
+router.post("/add", (req, res) => {
+  res.send({ message: "add website" });
+});
+
+// delete website from all remote servers
+router.delete("/delete", (req, res) => {
+  res.send({ message: "delete_website" });
 });
 
 module.exports = router;
