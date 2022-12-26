d54155fb6d19bf9eae009106d1487e23577f6c3f
ein <ein@jp9.org>
2022-12-26 09:47:43 -0800

    Skip isAuth for development

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -4,24 +4,30 @@ const { dbAll } = require("../database/database");
 isAuth = async (req, res, next) => {
   // const authToken = req.headers["x-access-token"];
 
-  const jwtSecretQuery = await dbAll(
-    `SELECT value FROM config  WHERE  key is 'jwtSecret'`
-  );
-
-  const jwtSecret = jwtSecretQuery[0].value;
-
-  const authToken = req.cookies["jwt"];
-
-  if (!authToken) {
-    return res.status(403).send({ message: "No token provided!" });
-  }
-
-  jwt.verify(authToken, jwtSecret, (err) => {
-    if (err) {
-      return res.status(401).send({ message: "Unauthorized!" });
-    }
+  if (process.env.NODE_ENV === "development") {
     next();
-  });
+  } else {
+    const jwtSecretQuery = await dbAll(
+      `SELECT value
+       FROM config
+       WHERE key is 'jwtSecret'`
+    );
+
+    const jwtSecret = jwtSecretQuery[0].value;
+
+    const authToken = req.cookies["jwt"];
+
+    if (!authToken) {
+      return res.status(403).send({ message: "No token provided!" });
+    }
+
+    jwt.verify(authToken, jwtSecret, (err) => {
+      if (err) {
+        return res.status(401).send({ message: "Unauthorized!" });
+      }
+      next();
+    });
+  }
 };
 
 const authStatus = isAuth;
