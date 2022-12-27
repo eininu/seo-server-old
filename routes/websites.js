13ed45d48208fd8396130440cac214992271ba53
ein <ein@jp9.org>
2022-12-26 17:34:33 -0800

    Test sync delete websites

--- routes/websites.js
+++ routes/websites.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const fs = require("fs");
 const decompress = require("decompress");
+const { dbAll } = require("../database/database");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -54,27 +55,59 @@ router.post("/", upload.any(), (req, res) => {
   res.send({ message: "ok" });
 });
 
-router.delete("/", async (req, res) => {
-  const website = req.body.website;
+router.delete(
+  "/",
+  async (req, res, next) => {
+    const website = req.body.website;
+    // initiator
+    // executor
 
-  const websites = fs
-    .readdirSync(process.cwd() + "/nginx-configs/")
-    .map((el) => {
-      return el.split(".conf")[0];
-    });
+    const websites = fs
+      .readdirSync(process.cwd() + "/nginx-configs/")
+      .map((el) => {
+        return el.split(".conf")[0];
+      });
 
-  if (websites.includes(website)) {
-    fs.unlinkSync(process.cwd() + "/nginx-configs/" + website + ".conf");
-    fs.unlinkSync(process.cwd() + "/websites/uploads/" + website + ".zip");
-    fs.rmSync(process.cwd() + "/websites/" + website, {
-      recursive: true,
-      force: true,
-    });
+    if (websites.includes(website)) {
+      fs.unlinkSync(process.cwd() + "/nginx-configs/" + website + ".conf");
+      fs.unlinkSync(process.cwd() + "/websites/uploads/" + website + ".zip");
+      fs.rmSync(process.cwd() + "/websites/" + website, {
+        recursive: true,
+        force: true,
+      });
 
-    res.send({ message: `${website} deleted` });
-  } else {
-    res.send({ message: `${website} doesn't exists` });
+      next();
+    } else {
+      res.send({ message: `${website} doesn't exists` });
+    }
+  },
+  async (req, res) => {
+    const website = req.body.website;
+    const serversFromDb = await dbAll(`SELECT t.* FROM servers t LIMIT 501`);
+    const servers = serversFromDb.map((el) => el.server_ip);
+    console.log(servers);
+
+    if (servers.includes(req.ip)) {
+      res.send({ message: `${website} deleted successfully` });
+    } else {
+      const log = Promise.all(
+        servers.map(async (server) => {
+          const request = await fetch(`http://${server}/api/websites/`, {
+            headers: {
+              accept: "*/*",
+              "accept-language": "en-US,en;q=0.9,ru;q=0.8",
+              "content-type": "application/json; charset=utf-8",
+            },
+            body: `{\"website\":\"${website}\"}`,
+            method: "DELETE",
+          });
+          return await request.json();
+        })
+      );
+
+      res.send({ message: `${website} deleted successfully`, log });
+    }
   }
-});
+);
 
 module.exports = router;
