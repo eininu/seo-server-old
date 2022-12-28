169b3e65f356e513ba5e4065447013c57e7caf4f
ein <ein@jp9.org>
2022-12-28 11:59:04 -0800

    Remove test nginx status

--- routes/shell/nginx.js
+++ routes/shell/nginx.js
@@ -19,14 +19,6 @@ const shell = require("shelljs");
 
 // sudo nginx -s reload
 
-router.get("/status/:password", async (req, res) => {
-  const password = req.params.password;
-  // const command = shell.exec("whoami");
-  const command = await shell.exec(`echo ${password} | sudo -S nginx -t`)
-    .stdout;
-  res.send({ message: `${command}` });
-});
-
 router.post("/reload", (req, res) => {
   const password = req.body.password;
   // const command = shell.exec("whoami");
