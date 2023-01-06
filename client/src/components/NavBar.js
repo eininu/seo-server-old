eb45709c99afb0d2a8743c7bbafae75688dc90dd
ein <ein@jp9.org>
2023-01-06 02:16:19 -0800

    Add className active working

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,13 @@
-import {useState} from "react";
-
-
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
+
+  const currentPathName = window.location.pathname;
+
+  useEffect(() => {
+    console.log(currentPathName);
+  });
 
   return (
     <div className="bg-primary-darker">
@@ -17,31 +21,69 @@ const NavBar = () => {
             data-class="d-none"
           >
             Menu
-            <i className="fa fa-bars" onClick={() => setMobileMenu(!mobileMenu)}></i>
+            <i
+              className="fa fa-bars"
+              onClick={() => setMobileMenu(!mobileMenu)}
+            ></i>
           </button>
         </div>
-        <div id="main-navigation" className={mobileMenu ? "d-lg-block mt-2 mt-lg-0" : "d-none d-lg-block mt-2 mt-lg-0"}>
+        <div
+          id="main-navigation"
+          className={
+            mobileMenu
+              ? "d-lg-block mt-2 mt-lg-0"
+              : "d-none d-lg-block mt-2 mt-lg-0"
+          }
+        >
           <ul className="nav-main nav-main-dark nav-main-horizontal nav-main-hover">
             <li className="nav-main-item">
-              <a className="nav-main-link active" href="/">
+              <a
+                className={
+                  currentPathName === "/"
+                    ? "nav-main-link active"
+                    : "nav-main-link"
+                }
+                href="/"
+              >
                 <i className="nav-main-link-icon si si-speedometer"></i>
                 <span className="nav-main-link-name">Dashboard</span>
               </a>
             </li>
             <li className="nav-main-item">
-              <a className="nav-main-link" href="/websites">
+              <a
+                className={
+                  currentPathName === "/websites"
+                    ? "nav-main-link active"
+                    : "nav-main-link"
+                }
+                href="/websites"
+              >
                 <i className="nav-main-link-icon si si-globe"></i>
                 <span className="nav-main-link-name">Websites</span>
               </a>
             </li>
             <li className="nav-main-item">
-              <a className="nav-main-link" href="/servers">
+              <a
+                className={
+                  currentPathName === "/servers"
+                    ? "nav-main-link active"
+                    : "nav-main-link"
+                }
+                href="/servers"
+              >
                 <i className="nav-main-link-icon fa fa-server"></i>
                 <span className="nav-main-link-name">Servers</span>
               </a>
             </li>
             <li className="nav-main-item">
-              <a className="nav-main-link" href="/settings">
+              <a
+                className={
+                  currentPathName === "/settings"
+                    ? "nav-main-link active"
+                    : "nav-main-link"
+                }
+                href="/settings"
+              >
                 <i className="nav-main-link-icon si si-settings"></i>
                 <span className="nav-main-link-name">Settings</span>
               </a>
